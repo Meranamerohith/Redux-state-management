09509bcc7aa2a8f42ae29aa2b362b0d6040229bf
meranamerohith <sairohith10@outllok.com>
2024-04-02 06:43:06 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'; 
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import bankStore from './Stores/BankStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Perform or dispatch an action :  CREATE
+bankStore.dispatch({type:"CREATE", amount : 0});  
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* Step3: Use <Provider/>  to share the store to components */}
+     <Provider store={bankStore}>
+        <App  />
+     </Provider>  
+  </React.StrictMode>    
+   
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    
+ 
